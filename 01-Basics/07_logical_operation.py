6b9e8cbcf015cd8ced344a0165fd3f437dd7b70e
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2026-02-06 15:16:38 +0530

    logical_operation

---
+++ 01-Basics/07_logical_operation.py
@@ -0,0 +1,7 @@
+#logical operations
+
+x=input("enter true or false for x:")=="True"
+y=input("enter true or false for y:")=="True"
+print("x and y:",x and y)
+print("x or y:",x or y)
+print("not y:",not y)
