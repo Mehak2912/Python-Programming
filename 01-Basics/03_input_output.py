84359b15311afc1c8a722a3dea031f750d67f4f3
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2026-02-06 14:43:05 +0530

    Add input/output example

---
+++ 01-Basics/03_input_output.py
@@ -0,0 +1,7 @@
+#input and ouput
+name=input("enter your name:")
+age=int(input("enter your age:"))
+print("name:",name)
+print("age:",age)
+print("type of name:",type(name))
+print("type of age:",type(age))
