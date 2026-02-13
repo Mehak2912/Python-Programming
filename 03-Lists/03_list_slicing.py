13a8ee3930c7f4d1ea977a674e28923f51d3a64b
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-02-13 15:18:39 +0530

    Create 03_list_slicing.py

---
+++ 03-Lists/03_list_slicing.py
@@ -0,0 +1,6 @@
+#list slicing
+
+items=input("enter items separated by space:").split()
+start=int (input("enter start index:"))
+end=int(input("enter end index:"))
+print("slice:",items[start:end])
