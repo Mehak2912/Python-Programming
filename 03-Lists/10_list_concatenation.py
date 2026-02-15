84217e1c1828922f3b6af0f276b5b589d4dc281b
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-02-15 16:59:04 +0530

    Create 10_list_concatenation.py

---
+++ 03-Lists/10_list_concatenation.py
@@ -0,0 +1,6 @@
+#list_concatenation
+
+l1=input("enter first list separated by space:").split()
+l2=input("enter second list separated by space:").split()
+concatenated=l1+l2
+print("Concatenated:",concatenated)
