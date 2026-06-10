b8120f6144b46cf9e53c68ddec9f8c273cbef61f
Mehak Dhamija <MEHAK.22835@stu.upes.ac.in>
2026-06-10 17:30:49 +0530

    Add tuple packing and unpacking example

---
+++ 04-Tuples/05_tuple_pack_unpack.py
@@ -0,0 +1,9 @@
+#tuple_packing_unpacking
+
+a=input("enter first value: ")
+b=input("enter second value: ")
+c=input("enter third value: ")
+my_tuple=(a,b,c)
+print("Packed tuple:", my_tuple)
+x,y,z=my_tuple
+print("Unpacked values:","x=",x,", y=",y,", z=",z)
